da466ff6b471b92a5ee5ec74b2c38805ece1e965
Jason Bowman <jbowman@g.hmc.edu>
2024-12-26 20:47:03 -0800

    initial attempt at I2C

--- main/main.ino
+++ main/main.ino
@@ -4,13 +4,16 @@
 
 //#include "motor_control.h"
 #include <Arduino.h>
-
-#define duty_cycle
+#include "../Arduino-I2C/I2C_8Bit.h"
 
 /****************
 * initial setup *
 ****************/
  void setup() {
+
+  // initialize and format the LSM6DS3 IMU
+  I2C_8Bit_begin(); 
+  void I2C_8Bit_writeToModule(uint8_t i2cAddr, uint8_t registerAddr, uint8_t data);
 
   // pin definitions
   pinMode(17, OUTPUT);
